86bb4046fa6cc9c52bcc8c5b580272628b34f998
Cezary Kuźma <79049815+czaaru@users.noreply.github.com>
2021-03-31 21:21:25 +0200

    chore: setup base route (#10)

--- src/app/koaApp.ts
+++ src/app/koaApp.ts
@@ -3,7 +3,7 @@ import { router } from './routes/router';
 
 export const app = new Koa();
 
-app.use(router.routes());
+app.use(router.routes()).use(router.allowedMethods());
 
 if (process.env.SERVER_PORT) {
   app.listen(process.env.SERVER_PORT);
