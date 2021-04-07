603ac0a363f28f12842c281a69148be7bbc2700f
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-04-07 19:33:54 +0200

    test: rewrite koaApp tests (#19)

--- src/app/koaApp.spec.ts
+++ src/app/koaApp.spec.ts
@@ -1,5 +1,42 @@
-describe('Mock test', () => {
-  it('should always be true', () => {
-    expect(true).toEqual(true);
+import { startService } from './koaApp';
+
+jest.mock('koa');
+
+const setServerPortEnv = (port: string) => {
+  const originalServerPort = process.env.SERVER_PORT;
+
+  process.env.SERVER_PORT = port;
+
+  return () => {
+    process.env.SERVER_PORT = originalServerPort;
+  };
+};
+
+const deleteServerPortEnv = () => {
+  const originalServerPort = process.env.SERVER_PORT;
+
+  delete process.env.SERVER_PORT;
+
+  return () => {
+    process.env.SERVER_PORT = originalServerPort;
+  };
+};
+
+describe('Koa app', () => {
+  it('should set the port if SERVER_PORT is set', () => {
+    const port = '5000';
+    const cleanup = setServerPortEnv(port);
+    const app = startService();
+    expect(app.listen).toHaveBeenCalledWith(port);
+
+    cleanup();
+  });
+
+  it('should not set the port if SERVER_PORT is not set', () => {
+    const cleanup = deleteServerPortEnv();
+    const app = startService();
+    expect(app.listen).not.toHaveBeenCalled();
+
+    cleanup();
   });
 });
