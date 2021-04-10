caa899f13e118c36a16f8db6770df3873ea5c290
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-04-10 20:12:53 +0200

    feat: add logging to datasets middleware

--- src/app/routes/datasets/middleware.ts
+++ src/app/routes/datasets/middleware.ts
@@ -1,14 +1,19 @@
+import { getLogger } from 'app/logger';
 import { Middleware } from 'koa';
 import { fetchSubjects } from '../../fetchers';
 import { limitSubjects, parseLimit } from './utils';
 
 const DEFAULT_LIMIT = 2;
 
+const LOG = getLogger(__filename);
+
 export const datasetsMiddleware: Middleware = async (ctx, next) => {
   if (!ctx.params.id) {
     ctx.throw(404);
     return;
   }
+
+  LOG.info('Fetching subjects for %s.', ctx.params.id);
 
   const response = await fetchSubjects(ctx.params.id);
 
@@ -19,7 +24,11 @@ export const datasetsMiddleware: Middleware = async (ctx, next) => {
 
   const subjects = response.body;
 
+  LOG.info('Parsing limit %s.', ctx.query.limit);
+
   const limit = parseLimit(ctx.query.limit) || DEFAULT_LIMIT;
+
+  LOG.info('Limiting subjects %s.');
   const limitedSubjects = limitSubjects(subjects, limit);
 
   ctx.body = {
