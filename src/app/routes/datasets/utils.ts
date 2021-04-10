48457855c09fcbd1915f1eb0f6e64d66efb74479
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-04-10 20:03:32 +0200

    feat: improve performance by limit only by keys maybe?

--- src/app/routes/datasets/utils.ts
+++ src/app/routes/datasets/utils.ts
@@ -9,7 +9,7 @@ export const parseLimit = (limit?: string | string[]) => {
   return !Number.isNaN(parsedLimit) ? parsedLimit : undefined;
 };
 
-export const limitSubjects = (dataset: Subjects, limit: number) =>
-  Object.entries(dataset)
+export const limitSubjects = (subjects: Subjects, limit: number) =>
+  Object.keys(subjects)
     .slice(0, limit)
-    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+    .reduce((acc, key) => ({ ...acc, [key]: subjects[key] }), {});
